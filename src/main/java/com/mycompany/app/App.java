ddbb0def6aaeca7a36ade6fe034c2aa94c82b9db
MarceloBuestan <cirinomarcelo69@gmail.com>
2026-06-18 03:48:57 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app;
+
+import Int.Pantalla;
+
+/**
+ *
+ * @author marce
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Pantalla F1 = new Pantalla();
+        F1.setVisible(true);
+    }
+}
